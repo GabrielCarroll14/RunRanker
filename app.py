e9d2cb40df77e13f916c963229dc790b3858756f
Gabriel Carroll <gabrielcarroll11@gmail.com>
2025-03-19 21:51:17 +0000

    Build the files
    Add the first file structrure with a little code in the files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ranks', methods=['GET', 'POST'])
+def ranks():
+    return render_template('ranks.html')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
