0b7fdfe3c1c6c4c0343bf435a4dfa695145a3087
Gabriel Carroll <gabrielcarroll11@gmail.com>
2025-03-20 08:37:31 +0000

    Add the ranking system
    I have added the ranking system to the 'app.py' file.

--- app.py
+++ app.py
@@ -12,14 +12,32 @@ def submit():
     
     # Get the name and time from the form
     name = request.form['name']
-    time = request.form['time']
+    time = int(request.form['time'])
     
     # Write the name and time to a file
     with open("times.txt", "a") as f:
-        f.write(name + " " + time + "\n")
+        f.write(name + " " + str(time) + "\n")
+    
+    # The ranking system
+    if time >= 35:
+        rank = "Bronze Strider"
+    elif time >= 30:
+        rank = "Silver Sprinter"
+    elif time >= 27:
+        rank = "Gold Pacer"
+    elif time >= 24:
+        rank = "Platinum Racer"
+    elif time >= 21:
+        rank = "Diamond Dasher"
+    elif time >= 19:
+        rank = "Elite Striker"
+    elif time >= 17:
+        rank = "Champion Runner"
+    else:
+        rank = "Legendary Phantom"
     
     # Redirect to the ranks page with the name and time attched
-    return render_template('ranks.html', name=name, time=time)
+    return render_template('ranks.html', name=name, time=time, rank=rank)
     
 
 if __name__ == '__main__':
